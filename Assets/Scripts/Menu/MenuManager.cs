17ae42ec8e49281206b9446bb34a0f6b4fff5c80
Zagorouiko <Zagorouiko@gmail.com>
2020-06-07 15:14:07 -0700

    added photon networking, replaced camera rig with updated steamvr prefab

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jenga.Menus
+{
+    public class MenuManager : MonoBehaviour
+    {
+        public Panel currentPanel = null;
+
+        private List<Panel> panelHistory = new List<Panel>();
+
+        private void Start()
+        {
+            SetupPanels();
+        }
+
+        private void SetupPanels()
+        {
+            Panel[] panels = GetComponentsInChildren<Panel>();
+
+            foreach (Panel panel in panels)
+            {
+                panel.Setup(this);
+            }
+
+            currentPanel.Show();
+        }
+    }
+}
+
---
+++ Assets/Scripts/Menu/PlayerNameInput.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+using Photon.Pun;
+
+namespace Jenga.Menus
+{
+    public class PlayerNameInput : MonoBehaviour
+    {
+        [SerializeField] private TMP_InputField nameInputField = null;
+        [SerializeField] private Button continueButton = null;
+
+        private const string PlayerPrefsNameKey = "PlayerName";
+        private void Start()
+        {
+            SetupInputField();
+        }
+
+        private void SetupInputField()
+        {
+            if (!PlayerPrefs.HasKey(PlayerPrefsNameKey)) { return; }
+
+            string defaultName = PlayerPrefs.GetString(PlayerPrefsNameKey);
+
+            nameInputField.text = defaultName;
+
+            SetPlayerName(defaultName);
+        }
+
+        private void SetPlayerName(string name)
+        {
+            continueButton.interactable = !string.IsNullOrEmpty(name);
+        }
+
+        public void SavePlayerName()
+        {
+            string playerName = nameInputField.text;
+
+            PhotonNetwork.NickName = playerName;
+
+            PlayerPrefs.SetString(PlayerPrefsNameKey, playerName);
+        }
+    }
+}
+
